53e5ad4e3303921db81a37dcf5cb9d967979e81e
Franco A <francoag.ntorres@gmail.com>
2020-11-27 13:37:50 -0300

    Add randstad

---
+++ randstad/tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0F1941',
+        primary: '#6BA5B4',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
