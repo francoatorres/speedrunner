e500021181d79a1a27bccd5c9659c2e3de584235
Franco A <francoag.ntorres@gmail.com>
2020-11-26 23:55:53 -0300

    Add sebo

---
+++ sebo/tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6937FF',
+        'primary-cont': '#784BFF',
+        secondary: '#00ADFF',
+        text: '#221e41',
+      },
+      borderRadius: {
+        exacly: '50px',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
