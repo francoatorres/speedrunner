821132c5b1eec9cbdfc0ba26643af501fb1bfe0f
Franco A <francoag.ntorres@gmail.com>
2020-11-27 22:37:25 -0300

    Add discord

---
+++ discord/tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        dark: '#23272a',
+        primary: '#7289DA',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
