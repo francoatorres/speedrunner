c4e8d864cc856485ced013c6fe4dd08a98f0d0ec
Franco A <francoag.ntorres@gmail.com>
2020-11-28 20:45:03 -0300

    Add vivero

---
+++ vivero/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        dark: '#023A15',
+        primary: '#F09C96',
+        'primary-bg': '#FFE6E2',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
