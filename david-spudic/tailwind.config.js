6f4a47821b9aef3bdefa02b2042752e0277d6e47
Franco A <francoag.ntorres@gmail.com>
2020-11-29 16:27:39 -0300

    Add david-spudic

---
+++ david-spudic/tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        light: '#F3F3F1',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
---
+++ dia/tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        light: '#C6D1D8',
+        beish: '#F3EEE7',
+        blue: '#C6D1D8',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
