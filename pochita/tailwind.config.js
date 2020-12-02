34040da36a40df2981dd66190725604925c8292b
Franco A <francoag.ntorres@gmail.com>
2020-12-02 13:32:05 -0300

    Add pochita

---
+++ pochita/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        light: '#F3F3F3',
+        dark: '#efefef',
+        nav: '#6B6B6B',
+        red: '#E0020F',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
